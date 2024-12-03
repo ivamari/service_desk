9b7e6b0c6bcebefdd6424403d9cfa1c1deb2add0
ivameri <ivamerii@yandex.ru>
2024-12-03 20:05:29 +0200

    add Ticket model to tickets app

--- tickets/models/tickets.py
+++ tickets/models/tickets.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class TicketStatus(models.Model):
@@ -11,3 +14,19 @@ class TicketStatus(models.Model):
     def __str__(self):
         return f'{self.code} ({self.name})'
 
+
+class Ticket(models.Model):
+    client = models.ForeignKey(User, models.SET_NULL, 'client_tickets',
+                               null=True, verbose_name='Клиент')
+    status = models.ForeignKey(TicketStatus, models.RESTRICT,
+                               'tickets', verbose_name='Статус')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    manager = models.ForeignKey(User, models.SET_NULL, 'manager_tickets',
+                                null=True, verbose_name='Менеджер')
+
+    class Meta:
+        verbose_name = 'Обращение'
+        verbose_name_plural = 'Обращения'
+
+    def __str__(self):
+        return f'Обращение №{self.pk} - {self.status}'
